3470bd4f039e688206caec6cff0902f3b100bf6f
ayushkanha <61038773+ayushkanha@users.noreply.github.com>
2025-09-21 16:50:25 +0530

    Update ContactPage.jsx

--- Frontend/src/pages/ContactPage.jsx
+++ Frontend/src/pages/ContactPage.jsx
@@ -16,7 +16,7 @@ const ContactPage = () => {
         }
 
         try {
-            const response = await fetch('http://localhost:8000/contact', {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/contact`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
